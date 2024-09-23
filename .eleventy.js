cde8833bbc2c1d21878d96e34cc9e011db41e391
Steven Bailey <stevenbailey@Macmini.attlocal.net>
2024-09-23 15:09:19 -0400

    added errors mock

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,30 @@
-const util = require('util');
+const htmlmin = require("html-minifier");
+const util = require("util");
+require("dotenv").config();
+const isProduction = process.env.ELEVENTY_ENV === `production`;
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/images");
 
+  // Minify HTML Output
+  eleventyConfig.addTransform("htmlmin", function (content, outputPath) {
+    // Eleventy 1.0+: use this.inputPath and this.outputPath instead
+    if (isProduction && outputPath && outputPath.endsWith(".html")) {
+      let minified = htmlmin.minify(content, {
+        useShortDoctype: true,
+        removeComments: true,
+        collapseWhitespace: true,
+      });
+      return minified;
+    }
+    return content;
+  });
+
   return {
     dir: {
-      input: "src",          // Set the input to the "src" directory
+      input: "src", // Set the input to the "src" directory
       includes: "_includes", // This tells Eleventy to look for _includes inside src/
-      output: "_site"
-    }
+      output: "_site",
+    },
   };
 };
